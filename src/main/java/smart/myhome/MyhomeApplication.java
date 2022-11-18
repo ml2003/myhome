61a14f8ca41988284f56bdca298fda4d7a68ceac
a.karmanova <ml2003@list.ru>
2022-11-18 09:45:56 +0300

    add swager

--- src/main/java/smart/myhome/MyhomeApplication.java
+++ src/main/java/smart/myhome/MyhomeApplication.java
@@ -3,11 +3,15 @@ package smart.myhome;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class MyhomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyhomeApplication.class, args);
     }
+
+
 
 }
